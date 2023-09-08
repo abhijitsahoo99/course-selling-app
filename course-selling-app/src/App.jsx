b16666e1deabee5bcf1bb068f2ff8eb4a178e51f
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2023-09-08 15:43:01 +0530

    Added appbar and updated app,jsx with router

--- course-selling-app/src/App.jsx
+++ course-selling-app/src/App.jsx
@@ -1,13 +1,22 @@
-import Signup from './Signup'
 import './App.css'
+import Signin from './Signin'
+import Signup from './Signup'
+import Appbar from './Appbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  
 
   return (
-    <>
-      <Signup  />
-    </>
+    <div>
+           <Router>
+             <Appbar />
+                <Routes>
+                    <Route path={"/signin"} element={<Signin />} />
+                    <Route path={"/signup"} element={<Signup />} />
+                </Routes>
+            </Router>
+     </div>
+    
   )
 }
 
