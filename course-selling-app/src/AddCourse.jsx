350f501947c2befe06eadb3256bc33618a8f95ae
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2023-09-09 07:59:46 +0530

    AddCourse jsx and css files added for admin to add new course

---
+++ course-selling-app/src/AddCourse.jsx
@@ -0,0 +1,52 @@
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import {Card, Typography} from '@mui/material';
+import {useState} from "react";
+import './AddCourse.css'
+
+function AddCourse() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [image, setImage] = useState("");
+    return <div>
+    <div className='courseheading'>
+        <Typography variant = {"h6"}>Please add your courses</Typography>
+       </div>
+        <div className='box'>
+            <Card className='card' variant="outlined">
+            <TextField onChange = {(e)=> setTitle(e.target.value)} 
+            sx={{paddingBottom : 2}} fullWidth={true} id="outlined-basic" label="Title" variant="outlined" />
+            <TextField onChange = {(e)=> setDescription(e.target.value)} 
+            sx={{paddingBottom : 2}} fullWidth={true} id="outlined-basic" label="Description" variant="outlined" />
+             <TextField onChange = {(e)=> setImage(e.target.value)} 
+            sx={{paddingBottom : 2}} fullWidth={true} id="outlined-basic" label="Image Link" variant="outlined" />
+
+            <Button size={"large"} variant="contained"
+             onClick = {() => {
+                function callback2(data){
+                    console.log(data)
+                    alert("Course Added")
+                }
+
+                function callback1(res){
+                    res.json().then(callback2)
+                }
+                fetch("http://localhost:3000/admin/courses", {
+                    method : "POST",
+                    body : JSON.stringify({
+                        title : title,
+                        description : description,
+                        imageLink: image,
+                        published: true
+                    }),
+                    headers : {
+                        "content-type": "application/json",
+                        "Authorization": "Bearer " + localStorage.getItem("token")
+                    }
+                }).then(callback1)
+            }}>Add Course</Button>
+            </Card>
+        </div>
+    </div>
+}
+export default AddCourse;
--- course-selling-app/src/App.jsx
+++ course-selling-app/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Signin from './Signin'
 import Signup from './Signup'
+import AddCourse from './AddCourse'
 import Appbar from './Appbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ function App() {
            <Router>
              <Appbar />
                 <Routes>
+                    <Route path={"/addcourse"} element={<AddCourse />} />
                     <Route path={"/signin"} element={<Signin />} />
                     <Route path={"/signup"} element={<Signup />} />
                 </Routes>
